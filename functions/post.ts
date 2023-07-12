d766b3c9a9d00f92cb7ccac9e2b2138520ee43c5
Abhinav Chalise <chalisezabhinav@gmail.com>
2023-07-12 13:05:14 +0545

    added function to query a single post

---
+++ functions/post.ts
@@ -0,0 +1,53 @@
+import { is } from "bluebird";
+import pool from "./db";
+import { Handler } from "@netlify/functions";
+
+interface PostData {
+  post_id: number;
+  title: string;
+  content: string;
+  author: string;
+  date: string;
+  upvotes: number;
+  downvotes: number;
+}
+
+export const handler: Handler = async (event, context) => {
+  if (event.httpMethod !== "GET") {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Invalid http method",
+      }),
+    };
+  }
+
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    const query = event.queryStringParameters;
+    if (query == null || isNaN(Number(query.id))) {
+      throw new Error("No id provided");
+    }
+    const [rows] = await pool.query(
+      `SELECT post_id, title, content, name as author, date, upvotes, downvotes FROM posts inner join userlogin on userlogin.email=posts.author inner join userdata on userdata.user_id = userlogin.id where post_id=${query.id};`
+    );
+    let data: PostData[] = rows as PostData[];
+    data.forEach((post) => {
+      post.content = post.content.replace(/&apos;/g, "'");
+      post.title = post.title.replace(/&apos;/g, "'");
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal server error",
+      }),
+    };
+  }
+};
